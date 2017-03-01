a46934ef3ed0a9ecc24e7d309b5b80a9223d7108
Jesus Ledezma <jledezma@student.neumont.edu>
2017-03-01 15:41:41 -0700

    Now loading in item from api url

--- src/gameFiles/Quiz.java
+++ src/gameFiles/Quiz.java
@@ -1,5 +1,19 @@
 package gameFiles;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Quiz {
-	
+	public static void main(String[] args) throws Exception{
+		   URL quiz = new URL("https://www.opentdb.com/api.php?amount=10&type=multiple");
+		   URLConnection connection = quiz.openConnection();
+		   BufferedReader readIn =  new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		   String line;
+		   while((line = readIn.readLine()) != null){
+			   System.out.println(line);
+		   }
+		   readIn.close();  
+	   }
 }
