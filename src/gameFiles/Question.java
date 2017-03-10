14ca05998612a202361c93a2e379aba6156cf1c1
Alex C <amcazar@yahoo.com>
2017-03-10 11:59:26 -0700

    question stuff

--- src/gameFiles/Question.java
+++ src/gameFiles/Question.java
@@ -1,5 +1,13 @@
 package gameFiles;
 
 public class Question{
+	String question;
+	String correctAnswer;
+	String[] wrongAnswers = new String[3];
 	
+	public Question(String ques, String correct, String[] incorrect){
+		question = ques;
+		correctAnswer = correct;
+		wrongAnswers = incorrect;
+	}
 }
