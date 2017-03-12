8dd3c1206ebdee676f575d384bbbf8f719e76a48
Jesus Ledezma <jledezma@student.neumont.edu>
2017-03-11 18:43:37 -0700

    did things, forgot what cuz taco bell

--- src/gameFiles/Options.java
+++ src/gameFiles/Options.java
@@ -1,5 +1,43 @@
 package gameFiles;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Options {
 	
+	public static URL generateURL(String category, String difficulty) throws MalformedURLException{
+		String categoryCode = "";
+		String difficultySetting = "";
+		switch(category){
+		case "Anime":
+			categoryCode = "31";
+			break;
+		case "Video Games":
+			categoryCode = "15";
+			break;
+		case "Movies":
+			categoryCode = "11";
+			break;
+		case "Mythology":
+			categoryCode = "20";
+			break;
+		case "Live Theater":
+			categoryCode = "13";
+			break;
+		}
+		switch(difficulty){
+		case "Easy":
+			difficulty = "easy";
+			break;
+		case "Medium":
+			difficulty = "medium";
+			break;
+		case "Hard":
+			difficulty = "hard";
+			break;
+		}
+		String url = "https://opentdb.com/api.php?amount=10&category=" + categoryCode + "&difficulty=" + difficultySetting + "&type=multiple";
+		URL quizURL = new URL(url);
+		return quizURL;
+	}
 }
